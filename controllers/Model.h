7725bcafe32ad58029239f1e66fc556d5306e45e
taldoflemis <coderflemis@gmail.com>
2023-10-21 06:56:27 -0300

    feat: make model results use a vector

--- controllers/Model.h
+++ controllers/Model.h
@@ -1,5 +1,6 @@
 #include "Function.h"
 #include "ModelResult.h"
+#include <vector>
 
 #ifndef Model_H
 #define Model_H
@@ -12,7 +13,7 @@ private:
     int maxIttr;
 
 public:
-    ModelResult* results;
+    std::vector<ModelResult*> results;
     Function* function;
     double firstStep;
     double nextStep;
@@ -24,7 +25,7 @@ public:
 
     void run();
     double getRoot();
-    ModelResult* getResults();
+    std::vector<ModelResult*> getResults();
     virtual void updateNextStep() = 0;
     virtual void updateValues() = 0;
     virtual void setFirstStep(double step) = 0;
