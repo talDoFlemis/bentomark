5fdee2b9b933982fd01561c9eb49de72c622876d
taldoflemis <coderflemis@gmail.com>
2023-10-21 06:46:30 -0300

    feat: add SharedMethodPageView

---
+++ views/shared/sharedmethodpagewidget.cpp
@@ -0,0 +1,59 @@
+#include "sharedmethodpagewidget.h"
+#include "ui_sharedmethodpagewidget.h"
+#include "../../controllers/ModelResult.h"
+using namespace std;
+
+SharedMethodPageWidget::SharedMethodPageWidget(std::string title, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::SharedMethodPageWidget)
+{
+    ui->setupUi(this);
+    QFont logoFont("AmazDooMRight", 70);
+    ui->title->setFont(logoFont);
+    ui->title->setText(QString::fromStdString(title));
+}
+
+void SharedMethodPageWidget::on_findRootBtn_clicked()
+{
+    ui->tableWidget->setRowCount(0);
+
+    double a = ui->aBox->value();
+    double epsilon = ui->epsilonBox->value();
+    double maxSteps = ui->maxStepsBox->value();
+    double initialGuess = ui->guessBox->value();
+
+    ModelResult* result = getResults(a, epsilon, initialGuess, maxSteps);
+    std::vector<ModelResult*> results;
+    results.push_back(result);
+    populateTable(results);
+}
+
+void SharedMethodPageWidget::populateTable(std::vector<ModelResult*> results){
+    for (int i = 0; i < results.size(); ++i) {
+        ModelResult* result = results[i];
+
+        ui->tableWidget->insertRow(i);
+        ui->tableWidget->setItem(i, 0,
+                                 new QTableWidgetItem(QString::number(result->root)));
+        ui->tableWidget->setItem(
+            i, 1, new QTableWidgetItem(QString::number(result->errorFunction)));
+        ui->tableWidget->setItem(
+            i, 2, new QTableWidgetItem(QString::number(result->errorInterval)));
+
+    }
+}
+
+void SharedMethodPageWidget::setupTable(){
+    ui->tableWidget->setColumnCount(3);
+    QStringList horizontalHeaders = {"xₖ",   "|f(xₖ)|",   "|xₖ+₁ - xₖ|"};
+    ui->tableWidget->setHorizontalHeaderLabels(horizontalHeaders);
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(
+        QHeaderView::Stretch);
+    ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
+}
+
+SharedMethodPageWidget::~SharedMethodPageWidget()
+{
+    delete ui;
+}
---
+++ views/shared/sharedmethodpagewidget.h
@@ -0,0 +1,29 @@
+#ifndef SHAREDMETHODPAGEWIDGET_H
+#define SHAREDMETHODPAGEWIDGET_H
+
+#include <QWidget>
+#include "../../controllers/ModelResult.h"
+
+namespace Ui {
+class SharedMethodPageWidget;
+}
+
+class SharedMethodPageWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SharedMethodPageWidget(std::string title, QWidget *parent = nullptr);
+    virtual ModelResult* getResults(double a, double epsilon, double initialGuess, int maxSteps) = 0;
+    ~SharedMethodPageWidget();
+
+private slots:
+    void on_findRootBtn_clicked();
+
+private:
+    Ui::SharedMethodPageWidget *ui;
+    void setupTable();
+    void populateTable(std::vector<ModelResult*> results);
+};
+
+#endif // SHAREDMETHODPAGEWIDGET_H
