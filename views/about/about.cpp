17570f87b533c1c3e1e8d792c9a7278bdbeb2528
Enzo <enzolozano90@gmail.com>
2023-10-30 20:22:42 -0300

    pronta

--- views/about/about.cpp
+++ views/about/about.cpp
@@ -6,6 +6,13 @@ AboutView::AboutView(QWidget *parent) :
     ui(new Ui::AboutView)
 {
     ui->setupUi(this);
+
+    // Crie a fonte com o nome "AmazDooMRight" e tamanho 80
+    QFont font("AmazDooMRight");
+    font.setPointSize(80);
+
+    // Aplique a fonte ao widget ou a um QLabel, por exemplo
+    ui->title->setFont(font);
 }
 
 AboutView::~AboutView()
